c6d12693c1080c0978fe8d520b15079953a1e22a
TRƯƠNG MINH HIẾU <1712443@student.hcmus.edu.vn>
2019-03-24 13:49:04 +0700

    112

--- Project1/CConvert.h
+++ Project1/CConvert.h
@@ -15,14 +15,19 @@ public:
 	//Chuyển đổi thập phân sang nhị phân
 	static string strDecToBin(string strDec);
 	static string strBinToDec(string strBin);
-
+	
+	//Chuyển chuỗi bit thường sang chuỗi bit bù 2
+	//Nếu truyền vào dãy bit của số 12 thì sẽ trả về chuỗi bit biểu diễn số -12
 	static string strBinTo2Complement(string str_src);
-	//Xóa bít 0
-	static void DelBit0(string &strSrc);
 
-	//
+	static void DelBit0(string &strSrc);
 	static void Reserve_Str(string &str_Src);
 };
+//Chia 2 giá trị của chuỗi  thập phân
 string _Div2_StrDec(string str_src);
+//Cộng 2 chuỗi thập phân
+string _Sum_strDec(string strDec1, string strDec2);
+//Trả về chuỗi giá trị của 2 lũy thừa K
+string _2Expn_K(int k);
 
 #endif // !__CConvert_H__
