f568c3973c54cc3dda4e27319bca8e29d471802b
Admin <Admin@NOSMILE>
2019-03-25 23:07:58 +0700

    Thêm file làm việc của từng người

--- Project1/Source/Main.cpp
+++ Project1/Source/Main.cpp
@@ -6,13 +6,9 @@ using namespace std;
 
 int main()
 {
-	
 	cout << CConvert::strDecToBin("123") << endl; // 10 -> 2
 	cout << CConvert::strBinToDec(CConvert::strDecToBin("65484686846684641684846464686")) << endl;
-	
 	//strDecToHec() và strHexToDec() sẻ dụng lại hàm trên nên việc test lại không cần thiểt!!!
-
-
 	system("pause");
 	return 0;
 }
---
+++ Project1/Source/QInt_Minh_Hieu.cpp
@@ -0,0 +1,2 @@
+﻿#include"QInt_16bytes.h"
+//Code của Minh Hiếu
